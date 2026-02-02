d3e9cfc3f9b2b1787bc0b49109c3299a97902df3
kummarianuradha-creator <kummarianuradha08@gmail.com>
2026-02-02 13:02:33 +0530

    Add model loading functionality to app.py
    Load machine learning model from file if it exists.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+
 from flask import Flask
+import os
+import pickle
 
 app = Flask(__name__)
+
+model = None
+model_path = os.path.join(os.getcwd(), "house_model.pkl")
+
+if os.path.exists(model_path):
+    model = pickle.load(open(model_path, "rb"))
+else:
+    print("model file not found")
 
 @app.route("/")
 def home():
@@ -8,3 +19,4 @@ def home():
 
 if __name__ == "__main__":
     app.run()
+
